00eb401e2ac794821aa2db6101a5d3f31add9a6a
Victor de Almeida Santana <80405995+VictorASantana@users.noreply.github.com>
2024-03-06 14:33:20 -0300

    setup do modelo do medico e serializer

--- backend/Doctor/models.py
+++ backend/Doctor/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Doctor(models.Model):
+    id_user = models.PositiveIntegerField(unique=True)
+    full_name = models.CharField(max_length=255, null=False, blank=False)
+    date_of_birth = models.DateField(blank=False, null=False)
+    email = models.EmailField(max_length=255, blank=False, unique=True, null=False)
+    phone_number = models.CharField(max_length=15, blank=True, default="")
+    document = models.CharField(max_length=15, unique=True, blank=False, null=True)
+
+    class Meta: 
+        db_table = "doctor"
